81a456531cd8c2be964dbdc5511cb45fc745dbb2
David Masip <david26694@gmail.com>
2023-05-31 14:19:51 +0000

    Typo

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("Hello, World!")
-    st.write("This is a basic Streamlit app.")
+    st.write("This is a basic Streamlit apps.")
 
 if __name__ == '__main__':
     main()
